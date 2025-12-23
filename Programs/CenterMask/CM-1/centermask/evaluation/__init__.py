deed020fdbd344e70f6cead39a370a8edeea6a21
bchuter <bentonchuter@gmail.com>
2025-12-22 20:31:49 -0600

    Initial commit: AxonClassNet codebase for verification

---
+++ Programs/CenterMask/CM-1/centermask/evaluation/__init__.py
@@ -0,0 +1,10 @@
+#MB: 03/31/2024
+#from .coco_evaluation import COCOEvaluator
+#from .cityscapes_evaluation import CityscapesInstanceEvaluator,CityscapesSemSegEvaluator
+# Copyright (c) Facebook, Inc. and its affiliates.
+from .axon_evaluation import COCOEvaluator
+from .axon_evaluator import DatasetEvaluator, DatasetEvaluators, inference_context, inference_on_dataset
+#from .cocoeval import COCOeval
+from centermask.evaluation.cocoeval import COCOeval
+
+__all__ = [k for k in globals().keys() if not k.startswith("_")]
